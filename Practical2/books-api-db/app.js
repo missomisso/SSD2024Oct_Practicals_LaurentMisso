816f1d41277314b22317c29571ae6103cb4e2a07
Misso667 <laurentmisso@gmail.com>
2024-12-21 11:30:29 +0800

    In Between Update

--- Practical2/books-api-db/app.js
+++ Practical2/books-api-db/app.js
@@ -1,12 +1,13 @@
+
 const express = require("express");
 const sql = require("mssql"); // Assuming you've installed mssql
 const dbConfig = require("./dbConfig");
-console.error("2:");
+
 
 const app = express();
-console.error("Database connection error:", err);
+
 const port = process.env.PORT || 3000; // Use environment variable or default port
-console.error("Database connection error:", err);
+
 app.listen(port, async () => {
   try {
     // Connect to the database
